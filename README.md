fdf84d89ca9b7073d4cdb37b0d3d7c593db02f3a
anojaryal <anoj1810@gmail.com>
2023-11-26 20:10:41 +0545

    repo clean

--- README.md
+++
@@ -1,7 +0,0 @@
-# mym_raktaveer
-
-A new Flutter project.
-
-## Getting Started
-
-This project is a starting point for a Flutter application.
--- android/app/src/main/kotlin/com/example/mym_raktaveer/MainActivity.kt
+++
@@ -1,6 +0,0 @@
-package com.example.mym_raktaveer
-
-import io.flutter.embedding.android.FlutterActivity
-
-class MainActivity: FlutterActivity() {
-}
