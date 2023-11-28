95172898b6b89b4debd2b5d05e3418aed47aee41
incri <paudelbaibhav0@gmail.com>
2023-11-28 21:20:45 +0545

    added more input fields on signup page

--- lib/signup_widget.dart
+++ lib/signup_widget.dart
@@ -21,89 +21,147 @@ class _SignUpWidgetState extends State<SignUpWidget> {
   final formKey = GlobalKey<FormState>();
   final emailController = TextEditingController();
   final passwordController = TextEditingController();
+  final fullnameController = TextEditingController();
+  final genderController = TextEditingController();
+  final ageController = TextEditingController();
+  final mobileNumberController = TextEditingController();
 
   @override
   void dispose() {
     emailController.dispose();
     passwordController.dispose();
+    fullnameController.dispose();
+    genderController.dispose();
+    ageController.dispose();
+    mobileNumberController.dispose();
     super.dispose();
   }
 
   @override
   Widget build(BuildContext context) {
     return Padding(
-        padding: const EdgeInsets.all(16.0),
-        child: Form(
-          key: formKey,
-          child: Column(
-            mainAxisAlignment: MainAxisAlignment.center,
-            children: [
-              // Email Input
-              TextFormField(
-                controller: emailController,
-                decoration: const InputDecoration(
-                  labelText: 'Email',
-                ),
-                autovalidateMode: AutovalidateMode.onUserInteraction,
-                validator: (email) =>
-                    email != null && !EmailValidator.validate(email)
-                        ? "Enter valid email"
-                        : null,
-                keyboardType: TextInputType.emailAddress,
+      padding: const EdgeInsets.all(16.0),
+      child: Form(
+        key: formKey,
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: [
+            // Fullname Input
+            TextFormField(
+              controller: fullnameController,
+              decoration: const InputDecoration(
+                labelText: 'Fullname',
               ),
-              const SizedBox(height: 16.0),
+              autovalidateMode: AutovalidateMode.onUserInteraction,
+              validator: (fullname) => fullname != null && fullname.isEmpty
+                  ? "Enter your fullname"
+                  : null,
+            ),
+            const SizedBox(height: 16.0),
 
-              // Password Input
-              TextFormField(
-                controller: passwordController,
-                decoration: const InputDecoration(
-                  labelText: 'Password',
-                ),
-                autovalidateMode: AutovalidateMode.onUserInteraction,
-                validator: (value) => value != null && value.length < 6
-                    ? "Password is too short"
-                    : null,
-                obscureText: true,
+            // Email Input
+            TextFormField(
+              controller: emailController,
+              decoration: const InputDecoration(
+                labelText: 'Email',
               ),
-              const SizedBox(height: 24.0),
+              autovalidateMode: AutovalidateMode.onUserInteraction,
+              validator: (email) =>
+                  email != null && !EmailValidator.validate(email)
+                      ? "Enter valid email"
+                      : null,
+              keyboardType: TextInputType.emailAddress,
+            ),
+            const SizedBox(height: 16.0),
 
-              // Sign-In Button
-              ElevatedButton(
-                onPressed: signUp,
-                child: const Text('Sign Up'),
+            // Gender Input
+            TextFormField(
+              controller: genderController,
+              decoration: const InputDecoration(
+                labelText: 'Gender',
               ),
+              autovalidateMode: AutovalidateMode.onUserInteraction,
+              validator: (gender) =>
+                  gender != null && gender.isEmpty ? "Enter your gender" : null,
+            ),
+            const SizedBox(height: 16.0),
 
-              const SizedBox(
-                height: 24,
+            // Age Input
+            TextFormField(
+              controller: ageController,
+              decoration: const InputDecoration(
+                labelText: 'Age',
               ),
+              autovalidateMode: AutovalidateMode.onUserInteraction,
+              validator: (age) =>
+                  age != null && age.isEmpty ? "Enter your age" : null,
+            ),
+            const SizedBox(height: 16.0),
 
-              RichText(
-                text: TextSpan(
-                  style:
-                      const TextStyle(color: Color.fromARGB(255, 56, 55, 55)),
-                  text: 'Already have an account? ',
-                  children: [
-                    TextSpan(
-                      recognizer: TapGestureRecognizer()
-                        ..onTap = () {
-                          // Check if the callback is not null before invoking it
-                          if (widget.onclickedSignIn != null) {
-                            // Pass the current context to the callback
-                            widget.onclickedSignIn!();
-                          }
-                        },
-                      text: "Sign In",
-                      style: const TextStyle(
-                        color: Color.fromARGB(255, 235, 34, 34),
-                        decoration: TextDecoration.underline,
-                      ),
+            // Mobile Number Input
+            TextFormField(
+              controller: mobileNumberController,
+              decoration: const InputDecoration(
+                labelText: 'Mobile Number',
+              ),
+              autovalidateMode: AutovalidateMode.onUserInteraction,
+              validator: (mobileNumber) =>
+                  mobileNumber != null && mobileNumber.isEmpty
+                      ? "Enter your mobile number"
+                      : null,
+              keyboardType: TextInputType.phone,
+            ),
+            const SizedBox(height: 16.0),
+
+            // Password Input
+            TextFormField(
+              controller: passwordController,
+              decoration: const InputDecoration(
+                labelText: 'Password',
+              ),
+              autovalidateMode: AutovalidateMode.onUserInteraction,
+              validator: (value) => value != null && value.length < 6
+                  ? "Password is too short"
+                  : null,
+              obscureText: true,
+            ),
+            const SizedBox(height: 24.0),
+
+            // Sign-Up Button
+            ElevatedButton(
+              onPressed: signUp,
+              child: const Text('Sign Up'),
+            ),
+
+            const SizedBox(
+              height: 24,
+            ),
+
+            RichText(
+              text: TextSpan(
+                style: const TextStyle(color: Color.fromARGB(255, 56, 55, 55)),
+                text: 'Already have an account? ',
+                children: [
+                  TextSpan(
+                    recognizer: TapGestureRecognizer()
+                      ..onTap = () {
+                        if (widget.onclickedSignIn != null) {
+                          widget.onclickedSignIn!();
+                        }
+                      },
+                    text: "Sign In",
+                    style: const TextStyle(
+                      color: Color.fromARGB(255, 235, 34, 34),
+                      decoration: TextDecoration.underline,
                     ),
-                  ],
-                ),
+                  ),
+                ],
               ),
-            ],
-          ),
-        ));
+            ),
+          ],
+        ),
+      ),
+    );
   }
 
   Future<void> signUp() async {
