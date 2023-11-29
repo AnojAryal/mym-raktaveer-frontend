7db217bcf8b3864d7a42718dcce174c1935b9df0
anojaryal <anoj1810@gmail.com>
2023-11-29 19:26:50 +0545

    phase 1

--- lib/Donor_Registration/progress_bar.dart
+++ lib/Donor_Registration/progress_bar.dart
@@ -1,22 +1,21 @@
 import 'package:flutter/material.dart';
 
 class MyProgressBar extends StatelessWidget {
-  const MyProgressBar({super.key});
+  const MyProgressBar({super. key});
+
   @override
   Widget build(BuildContext context) {
-    return Container(
-      padding: const EdgeInsets.all(16.0),
-      child: Column(
-        mainAxisAlignment: MainAxisAlignment.center,
-        children: [
-          const Text('Loading...'),
-          const SizedBox(height: 16.0),
-          LinearProgressIndicator(
-            backgroundColor: Colors.grey[200],
-            valueColor:const AlwaysStoppedAnimation<Color>(Colors.black),
-          ),
-        ],
-      ),
+    double progress = 0.5;
+
+    return Column(
+      children: [
+        LinearProgressIndicator(
+          backgroundColor: Colors.grey[200],
+          valueColor: const AlwaysStoppedAnimation<Color>(Colors.black),
+          value: progress,
+        ),
+        const SizedBox(height: 8.0),
+      ],
     );
   }
 }
--- lib/main.dart
+++ lib/main.dart
@@ -1,54 +1,24 @@
-import 'package:firebase_auth/firebase_auth.dart';
-import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
-import 'package:mym_raktaveer_frontend/auth_page.dart';
-import 'package:mym_raktaveer_frontend/utils.dart';
-import 'package:mym_raktaveer_frontend/verify_email.dart';
-import 'firebase_options.dart';
+import 'package:mym_raktaveer_frontend/Donor_Registration/Question1.dart';
 
-void main() async {
-  WidgetsFlutterBinding.ensureInitialized();
-  await Firebase.initializeApp(
-    options: DefaultFirebaseOptions.currentPlatform,
-  );
-  // Initialize Firebase
 
-  runApp(const MyApp());
+void main() {
+  runApp(MyApp());
 }
-
-final navigatorKey = GlobalKey<NavigatorState>();
 
 class MyApp extends StatelessWidget {
   const MyApp({super.key});
 
-  // This widget is the root of your application.
+ 
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      navigatorKey: navigatorKey,
-      scaffoldMessengerKey: Utils.messengerKey,
-      home: const MainPage(),
+      title: 'My Flutter App',
+      theme: ThemeData(
+        primarySwatch: Colors.blue,
+        visualDensity: VisualDensity.adaptivePlatformDensity,
+      ),
+      home:const UserChoicePage(), 
     );
   }
-}
-
-class MainPage extends StatelessWidget {
-  const MainPage({super.key});
-
-  @override
-  Widget build(BuildContext context) => Scaffold(
-          body: StreamBuilder<User?>(
-        stream: FirebaseAuth.instance.authStateChanges(),
-        builder: (context, snapshot) {
-          if (snapshot.connectionState == ConnectionState.waiting) {
-            return const Center(child: CircularProgressIndicator());
-          } else if (snapshot.hasError) {
-            return const Text("Something went wrong");
-          } else if (snapshot.hasData) {
-            return const VerifyEmailPage();
-          } else {
-            return const AuthPage();
-          }
-        },
-      ));
 }
