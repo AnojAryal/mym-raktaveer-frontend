a85ce1e0f9bfd22589706f06cb2dbeb6a8f52bad
anojaryal <anoj1810@gmail.com>
2023-12-25 14:48:54 +0545

    making device adaptive

--- lib/widgets/request_list_page.dart
+++ lib/widgets/request_list_page.dart
@@ -1,4 +1,4 @@
-// ignore_for_file: library_private_types_in_public_api, avoid_print
+// ignore_for_file: library_private_types_in_public_api
 
 import 'package:flutter/material.dart';
 import 'package:flutter_riverpod/flutter_riverpod.dart';
@@ -21,7 +21,6 @@ class _RequestListPageState extends ConsumerState<RequestListPage> {
     super.initState();
     WidgetsBinding.instance.addPostFrameCallback((_) {
       fetchBloodRequestData();
-      // Your function
     });
   }
 
@@ -38,6 +37,72 @@ class _RequestListPageState extends ConsumerState<RequestListPage> {
     });
   }
 
+  Widget buildBloodRequestCard(BloodRequestModel bloodRequest) {
+    return Padding(
+      padding: const EdgeInsets.symmetric(horizontal: 8.0, vertical: 4.0),
+      child: Card(
+        color: Colors.white,
+        child: Padding(
+          padding: const EdgeInsets.all(8.0),
+          child: Column(
+            crossAxisAlignment: CrossAxisAlignment.start,
+            children: <Widget>[
+              const SizedBox(height: 4),
+              Row(
+                children: <Widget>[
+                  const Text(
+                    'Quantity: ',
+                    style: TextStyle(
+                      fontSize: 16,
+                      fontWeight: FontWeight.w600,
+                      color: Colors.black,
+                    ),
+                  ),
+                  Text(
+                    bloodRequest.quantity,
+                    style: const TextStyle(fontSize: 16, color: Colors.red),
+                  ),
+                ],
+              ),
+              Row(
+                children: <Widget>[
+                  const Text(
+                    'Blood Group: ',
+                    style: TextStyle(
+                      fontSize: 16,
+                      fontWeight: FontWeight.w600,
+                      color: Colors.black,
+                    ),
+                  ),
+                  Text(
+                    '${bloodRequest.bloodGroupAbo} ${bloodRequest.bloodGroupRh}',
+                    style: const TextStyle(fontSize: 16, color: Colors.red),
+                  ),
+                ],
+              ),
+              Row(
+                children: <Widget>[
+                  const Text(
+                    'Urgency Level: ',
+                    style: TextStyle(
+                      fontSize: 16,
+                      fontWeight: FontWeight.w600,
+                      color: Colors.black,
+                    ),
+                  ),
+                  Text(
+                    bloodRequest.urgencyLevel,
+                    style: const TextStyle(fontSize: 16, color: Colors.red),
+                  ),
+                ],
+              ),
+            ],
+          ),
+        ),
+      ),
+    );
+  }
+
   @override
   Widget build(BuildContext context) {
     return Stack(
@@ -46,10 +111,10 @@ class _RequestListPageState extends ConsumerState<RequestListPage> {
           crossAxisAlignment: CrossAxisAlignment.start,
           children: <Widget>[
             const Padding(
-              padding: EdgeInsets.all(16.0),
+              padding: EdgeInsets.all(8.0),
               child: Text(
                 'Request Details',
-                style: TextStyle(fontSize: 20, fontWeight: FontWeight.bold),
+                style: TextStyle(fontSize: 20, fontWeight: FontWeight.w600),
               ),
             ),
             Expanded(
@@ -57,67 +122,7 @@ class _RequestListPageState extends ConsumerState<RequestListPage> {
                 itemCount: bloodRequestList.length,
                 itemBuilder: (context, index) {
                   final bloodRequest = bloodRequestList[index];
-                  return Card(
-                    child: Padding(
-                      padding: const EdgeInsets.all(16.0),
-                      child: Column(
-                        crossAxisAlignment: CrossAxisAlignment.start,
-                        children: <Widget>[
-                          const SizedBox(height: 8),
-                          Row(
-                            children: <Widget>[
-                              const Text(
-                                'Quantity: ',
-                                style: TextStyle(
-                                    fontSize: 16,
-                                    fontWeight: FontWeight.bold,
-                                    color: Colors.black),
-                              ),
-                              Text(
-                                bloodRequest.quantity,
-                                style: const TextStyle(
-                                    fontSize: 16, color: Colors.red),
-                              ),
-                            ],
-                          ),
-                          const SizedBox(height: 8),
-                          Row(
-                            children: <Widget>[
-                              const Text(
-                                'Blood Group: ',
-                                style: TextStyle(
-                                    fontSize: 16,
-                                    fontWeight: FontWeight.bold,
-                                    color: Colors.black),
-                              ),
-                              Text(
-                                '${bloodRequest.bloodGroupAbo} ${bloodRequest.bloodGroupRh}',
-                                style: const TextStyle(
-                                    fontSize: 16, color: Colors.red),
-                              ),
-                            ],
-                          ),
-                          const SizedBox(height: 8),
-                          Row(
-                            children: <Widget>[
-                              const Text(
-                                'Urgency Level: ',
-                                style: TextStyle(
-                                    fontSize: 16,
-                                    fontWeight: FontWeight.bold,
-                                    color: Colors.black),
-                              ),
-                              Text(
-                                bloodRequest.urgencyLevel,
-                                style: const TextStyle(
-                                    fontSize: 16, color: Colors.red),
-                              ),
-                            ],
-                          ),
-                        ],
-                      ),
-                    ),
-                  );
+                  return buildBloodRequestCard(bloodRequest);
                 },
               ),
             ),
