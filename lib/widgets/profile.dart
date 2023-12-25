e1b04c80ee85c8e17a988058da4363d481056081
anojaryal <anoj1810@gmail.com>
2023-12-25 14:52:34 +0545

    profile text used

--- lib/widgets/profile.dart
+++ lib/widgets/profile.dart
@@ -4,6 +4,8 @@ import 'package:mym_raktaveer_frontend/widgets/background.dart';
 import 'package:mym_raktaveer_frontend/widgets/profile_text.dart';
 
 class Profile extends StatelessWidget {
+  const Profile({super.key});
+
   @override
   Widget build(BuildContext context) {
     final Map<String, dynamic> responseData =
@@ -299,38 +301,6 @@ class Profile extends StatelessWidget {
             ),
           ],
         ),
-      ),
-    );
-  }
-}
-
-class CustomRichText extends StatelessWidget {
-  final String label;
-  final String value;
-
-  const CustomRichText({
-    super.key,
-    required this.label,
-    required this.value,
-  });
-
-  @override
-  Widget build(BuildContext context) {
-    return RichText(
-      text: TextSpan(
-        style: const TextStyle(
-          fontSize: 16,
-          color: Colors.black,
-        ),
-        children: [
-          TextSpan(
-            text: '$label: ',
-            style: const TextStyle(
-              fontWeight: FontWeight.w500,
-            ),
-          ),
-          TextSpan(text: value),
-        ],
       ),
     );
   }
