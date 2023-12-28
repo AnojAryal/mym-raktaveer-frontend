b1b8fe7a1ac68bdc230013368df54cf127fcf2e4
incri <paudelbaibhav0@gmail.com>
2023-12-29 00:08:44 +0545

    added application/json as accepted

--- lib/services/api_service.dart
+++ lib/services/api_service.dart
@@ -16,7 +16,10 @@ class ApiService {
     try {
       final response = await http.post(
         Uri.parse(fullUrl),
-        headers: {'Content-Type': 'application/json'},
+        headers: {
+          'Content-Type': 'application/json',
+          'Accept': 'application/json',
+        },
         body: jsonEncode(data),
       );
 
@@ -25,6 +28,7 @@ class ApiService {
       } else {
         // Handle different status codes appropriately
         print('Failed to post data. Status code: ${response.statusCode}');
+        print(response.body);
         return null; // Consider returning a more descriptive error object
       }
     } catch (error) {
@@ -46,6 +50,7 @@ class ApiService {
         headers: {
           'Content-Type': 'application/json',
           'Authorization': 'Bearer $jwtToken',
+          'Accept': 'application/json',
         },
         body: jsonEncode(data),
       );
@@ -65,22 +70,25 @@ class ApiService {
 
   Future<Map<String, dynamic>?> getData(String apiUrl, WidgetRef ref) async {
     final String fullUrl = "$baseUrl/$apiUrl";
+    final userData = ref.watch(userDataProvider);
 
     try {
-      final userData = ref.watch(userDataProvider);
       final jwtToken = userData?.accessToken;
+      print(jwtToken);
       final response = await http.get(
         Uri.parse(fullUrl),
         headers: {
+          'Authorization': 'Bearer $jwtToken',
           'Content-Type': 'application/json',
-          'Authorization':
-              'Bearer $jwtToken', // Include the JWT token in the 'Authorization' header
+          'Accept': 'application/json',
+// Include the JWT token in the 'Authorization' header
         },
       );
       if (response.statusCode == 200) {
         return json.decode(response.body);
       } else {
         print('Failed to get data. Status code: ${response.statusCode}');
+        print(response.body);
         return null; // Consider a more descriptive error object or throwing an exception
       }
     } catch (error) {
