922312b3791021a9b2b2f66e6cb0ea81bb685f3d
incri <paudelbaibhav0@gmail.com>
2023-12-29 20:07:06 +0545

    adding logic to fetch user location

--- lib/widgets/location_fetcher.dart
+++ lib/widgets/location_fetcher.dart
@@ -1,110 +1,29 @@
-import 'package:flutter/material.dart';
-import 'package:geocoding/geocoding.dart';
 import 'package:geolocator/geolocator.dart';
 
-class CurrentLocation extends StatelessWidget {
-  const CurrentLocation({super.key});
-
-  @override
-  Widget build(BuildContext context) {
-    return const MaterialApp(
-      home: LocationScreen(),
-    );
+Future<void> fetchLocation() async {
+  bool serviceEnabled = await Geolocator.isLocationServiceEnabled();
+  if (!serviceEnabled) {
+    // Handle location service not enabled.
+    return;
   }
-}
 
-class LocationScreen extends StatefulWidget {
-  const LocationScreen({super.key});
-
-  @override
-  // ignore: library_private_types_in_public_api
-  _LocationScreenState createState() => _LocationScreenState();
-}
-
-class _LocationScreenState extends State<LocationScreen> {
-  Position? _currentLocation;
-  late bool servicePermission = false;
-  late LocationPermission permission;
-
-  String _currentAddress = "";
-
-  Future<Position> _getCurrentLocation() async {
-    bool serviceEnabled;
-    LocationPermission permission;
-
-    serviceEnabled = await Geolocator.isLocationServiceEnabled();
-
-    if (!serviceEnabled) {
-      print("Service Disabled");
-    }
-    permission = await Geolocator.checkPermission();
+  LocationPermission permission = await Geolocator.checkPermission();
+  if (permission == LocationPermission.denied) {
+    permission = await Geolocator.requestPermission();
     if (permission == LocationPermission.denied) {
-      permission = await Geolocator.requestPermission();
-    }
-    return await Geolocator.getCurrentPosition();
-  }
-
-  _getAddressFromCoordinates() async {
-    try {
-      List<Placemark> placemarks = await placemarkFromCoordinates(
-          _currentLocation!.latitude, _currentLocation!.longitude);
-
-      Placemark place = placemarks[0];
-      setState(() {
-        _currentAddress =
-            "${place.street} ${place.subLocality}, ${place.locality}, ${place.administrativeArea}";
-      });
-    } catch (e) {
-      print(e);
+      // Permissions are denied, next time you could try requesting permissions again.
+      return;
     }
   }
 
-  @override
-  Widget build(BuildContext context) {
-    return Scaffold(
-        appBar: AppBar(
-          title: const Text("Get User Location"),
-          centerTitle: true,
-        ),
-        body: Center(
-            child: Column(
-          mainAxisAlignment: MainAxisAlignment.center,
-          crossAxisAlignment: CrossAxisAlignment.center,
-          children: [
-            const Text(
-              "Location Coordinates",
-              style: TextStyle(fontSize: 24, fontWeight: FontWeight.bold),
-            ),
-            const SizedBox(
-              height: 8,
-            ),
-            Text(
-                "Latitude = ${_currentLocation?.latitude}, Longitude = ${_currentLocation?.longitude}"),
-            const SizedBox(
-              height: 25,
-            ),
-            const Text(
-              "Location Address",
-              style: TextStyle(fontSize: 24, fontWeight: FontWeight.bold),
-            ),
-            const SizedBox(
-              height: 8,
-            ),
-            Text("${_currentAddress}"),
-            const SizedBox(
-              height: 25,
-            ),
-            ElevatedButton(
-                onPressed: () async {
-                  //get current location here
-                  _currentLocation = await _getCurrentLocation();
-                  await _getAddressFromCoordinates();
-
-                  print("${_currentLocation}");
-                  print("${_currentAddress}");
-                },
-                child: Text("Get Location")),
-          ],
-        )));
+  if (permission == LocationPermission.deniedForever) {
+    // Permissions are denied forever, handle appropriately.
+    return;
   }
+
+  // When we reach here, permissions are granted and we can fetch the location.
+  Position position = await Geolocator.getCurrentPosition(
+      desiredAccuracy: LocationAccuracy.high);
+  print("Latitude: ${position.latitude}, Longitude: ${position.longitude}");
+  // Implement your logic to handle the location data, e.g., sending it to your server.
 }
