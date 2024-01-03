b457889c438337b40d4ef816b8efa87185daa21b
KRSL367 <krishalpudasaini12345@gmail.com>
2024-01-04 01:03:53 +0545

    Donor List Page Ui Created with InkWell

--- lib/screens/donor/donor_list.dart
+++ lib/screens/donor/donor_list.dart
@@ -3,6 +3,7 @@ import 'package:mym_raktaveer_frontend/services/api_service.dart';
 import 'package:flutter_riverpod/flutter_riverpod.dart';
 import '../../services/blood_request_service.dart';
 import '../../widgets/background.dart';
+import '../../widgets/blood_request_detail.dart';
 
 class DonorList extends ConsumerWidget {
   final Map<String, dynamic> response;
@@ -77,98 +78,105 @@ class DonorList extends ConsumerWidget {
     );
   }
 
-  Card buildParticipantCard(Map<String, dynamic> participant) {
-    return Card(
-      color: Colors.white,
-      shape: RoundedRectangleBorder(
-        borderRadius: BorderRadius.circular(5),
-      ),
-      elevation: 5,
-      margin: const EdgeInsets.symmetric(vertical: 8),
-      child: ListTile(
-        key: UniqueKey(),
-        leading: const CircleAvatar(
-          radius: 20,
-          backgroundColor: Colors.white,
-          child: Icon(
-            Icons.person,
-            size: 20,
-            color: Color(0xFFFD1A00), // Adjust the icon color
-          ), // Adjust the background color of the circle
+  InkWell buildParticipantCard(Map<String, dynamic> participant) {
+    return InkWell(
+      onTap: () {
+        // Navigator.of(context).push(MaterialPageRoute(
+        //   builder: (context) => BloodRequestDetail(requestId: bloodRequest.id),
+        // ));
+      },
+      child: Card(
+        color: Colors.white,
+        shape: RoundedRectangleBorder(
+          borderRadius: BorderRadius.circular(5),
         ),
-        title: Row(
-          children: [
-            Text(
-              '${participant['full_name']}',
-              style: const TextStyle(
-                fontWeight: FontWeight.w500,
-                fontSize: 16,
-              ),
-            ),
-            const SizedBox(
-              width: 20,
-            ),
-            Text(
-              '${participant['gender']}',
-              style: const TextStyle(
-                fontStyle: FontStyle.normal,
-                color: Color.fromARGB(255, 78, 78, 78),
-              ),
-            ),
-          ],
-        ),
-        subtitle: Column(
-          crossAxisAlignment: CrossAxisAlignment.start,
-          children: [
-            const SizedBox(height: 8),
-            Row(
-              children: [
-                const Text(
-                  'Age: ',
-                  style: TextStyle(
-                      fontSize: 14,
-                      color: Colors.black,
-                      fontWeight: FontWeight.w500),
+        elevation: 5,
+        margin: const EdgeInsets.symmetric(vertical: 8),
+        child: ListTile(
+          key: UniqueKey(),
+          leading: const CircleAvatar(
+            radius: 20,
+            backgroundColor: Colors.white,
+            child: Icon(
+              Icons.person,
+              size: 20,
+              color: Color(0xFFFD1A00), // Adjust the icon color
+            ), // Adjust the background color of the circle
+          ),
+          title: Row(
+            children: [
+              Text(
+                '${participant['full_name']}',
+                style: const TextStyle(
+                  fontWeight: FontWeight.w500,
+                  fontSize: 16,
                 ),
-                const SizedBox(
-                  width: 4,
+              ),
+              const SizedBox(
+                width: 20,
+              ),
+              Text(
+                '${participant['gender']}',
+                style: const TextStyle(
+                  fontStyle: FontStyle.normal,
+                  color: Color.fromARGB(255, 78, 78, 78),
                 ),
-                Text(
-                  '${participant['age']}',
-                  style: const TextStyle(
-                    fontSize: 14,
-                    color: Color.fromARGB(255, 78, 78, 78),
+              ),
+            ],
+          ),
+          subtitle: Column(
+            crossAxisAlignment: CrossAxisAlignment.start,
+            children: [
+              const SizedBox(height: 8),
+              Row(
+                children: [
+                  const Text(
+                    'Age: ',
+                    style: TextStyle(
+                        fontSize: 14,
+                        color: Colors.black,
+                        fontWeight: FontWeight.w500),
                   ),
-                ),
-              ],
-            ),
-            const SizedBox(
-              height: 8,
-            ),
+                  const SizedBox(
+                    width: 4,
+                  ),
+                  Text(
+                    '${participant['age']}',
+                    style: const TextStyle(
+                      fontSize: 14,
+                      color: Color.fromARGB(255, 78, 78, 78),
+                    ),
+                  ),
+                ],
+              ),
+              const SizedBox(
+                height: 8,
+              ),
 
-            Row(
-              children: [
-                const Text(
-                  'Email: ',
-                  style: TextStyle(
-                      fontSize: 14,
-                      color: Colors.black,
-                      fontWeight: FontWeight.w500),
-                ),
-                const SizedBox(
-                  width: 4,
-                ),
-                Text(
-                  '${participant['email']}',
-                  style: const TextStyle(
-                    fontSize: 14,
-                    color: Color.fromARGB(255, 78, 78, 78),
+              Row(
+                children: [
+                  const Text(
+                    'Email: ',
+                    style: TextStyle(
+                        fontSize: 14,
+                        color: Colors.black,
+                        fontWeight: FontWeight.w500),
                   ),
-                ),
-              ],
-            ),
-            // Add more details as needed
-          ],
+                  const SizedBox(
+                    width: 4,
+                  ),
+                  Text(
+                    '${participant['email']}',
+                    style: const TextStyle(
+                      fontSize: 14,
+                      color: Color.fromARGB(255, 78, 78, 78),
+                    ),
+                  ),
+                ],
+              ),
+              // Add more details as needed
+            ],
+          ),
         ),
       ),
     );
